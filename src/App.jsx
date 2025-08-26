cd19f6aa09c8745d7b532ff46ebf07d29422aa61
Manisha Agrawal <manisha.agrawal@impelsys.com>
2025-08-26 18:52:05 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+
+      </div>
+      
+      <div className="card">
+        <h1 className="text-3xl font-bold underline border-2">
+          Hello world!
+        </h1>
+      </div>
+
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // adjust if you’re using Vue/Svelte/etc.
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
